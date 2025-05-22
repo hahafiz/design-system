ad532b0848bfc301ac6ccff25719043e2f4a2968
hahafiz <hafizqamarul19@gmail.com>
2025-05-22 23:37:59 +0800

    first commit

---
+++ src/components/Button.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Button = () => {
+  return (
+    <button
+      className="px-4 py-2 bg-slate-700 hover:bg-slate-800 rounded-md text-white"
+      type="button"
+    >
+      Buttons
+    </button>
+  );
+};
+
+export default Button;
