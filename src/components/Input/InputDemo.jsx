8c5f6511047871cf6f610cfdfaf665b177ebe849
hahafiz <hafizqamarul19@gmail.com>
2025-05-30 01:28:36 +0800

    progress: input component

--- src/components/Input/InputDemo.jsx
+++ src/components/Input/InputDemo.jsx
@@ -9,12 +9,12 @@ const InputDemo = () => {
       <div className="space-y-4">
         <h2 className="text-xl font-semibold">Input Types</h2>
         <div className="flex flex-wrap gap-4">
-          <Input type="checkbox" placeholder="Checkbox" className="w-4 h-4" />
+          <Input type="checkbox" placeholder="Checkbox" className="w-4" />
           <Input
             type="color"
             placeholder="Color input"
             style={{ height: "40px" }}
-            className="w-12"
+            className="w-12 pl-0"
           />
           <Input type="date" placeholder="Date input" />
           <Input type="email" placeholder="Email input" />
